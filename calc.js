3f63f60801a8afedc611f595cd8e2a7223b9d87c
Nasim Ahmad Khan <40820072+NakRex@users.noreply.github.com>
2021-08-06 15:28:31 +0530

    added function to two buttons

---
+++ calc.js
@@ -0,0 +1,16 @@
+
+
+console.log("hello");
+
+function getOne(){
+    let one = document.getElementById('one').value;
+    console.log(one);
+    var result = document.getElementById('result');
+    result.value += one ;
+}
+function getTwo(){
+    let two = document.getElementById('two').value;
+    console.log(two);
+    var result = document.getElementById('result');
+    result.value += two ;
+}
