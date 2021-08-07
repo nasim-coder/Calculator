dd7300a8bebf90b80923817c90fe8bc56dd35097
Nasim Ahmad Khan <40820072+NakRex@users.noreply.github.com>
2021-08-07 08:45:31 +0530

    addded functions to buttons

--- calc.js
+++ calc.js
@@ -8,9 +8,76 @@ function getOne(){
     var result = document.getElementById('result');
     result.value += one ;
 }
+
+
 function getTwo(){
     let two = document.getElementById('two').value;
     console.log(two);
     var result = document.getElementById('result');
     result.value += two ;
 }
+
+
+function getThree(){
+    let three = document.getElementById('three').value;
+    console.log(three);
+    var result = document.getElementById('result');
+    result.value += three ;
+}
+
+
+function getFour(){
+    let four = document.getElementById('four').value;
+    console.log(four);
+    var result = document.getElementById('result');
+    result.value += four ;
+}
+
+
+function getFive(){
+    let five = document.getElementById('five').value;
+    console.log(five);
+    var result = document.getElementById('result');
+    result.value += five ;
+}
+
+
+function getSix(){
+    let six = document.getElementById('six').value;
+    console.log(six);
+    var result = document.getElementById('result');
+    result.value += six ;
+}
+
+
+function getSeven(){
+    let seven = document.getElementById('seven').value;
+    console.log(seven);
+    var result = document.getElementById('result');
+    result.value += seven ;
+}
+
+
+function getEight(){
+    let eight = document.getElementById('eight').value;
+    console.log(eight);
+    var result = document.getElementById('result');
+    result.value += eight ;
+}
+
+
+function getNine(){
+    let nine = document.getElementById('nine').value;
+    console.log(nine);
+    var result = document.getElementById('result');
+    result.value += nine ;
+}
+
+
+function getZero(){
+    let zero = document.getElementById('zero').value;
+    console.log(zero);
+    var result = document.getElementById('result');
+    result.value += zero ;
+}
+
