471d51e284ac790655e0481cb0128ea02f451ae6
Nasim Ahamad Khan <40820072+NakRex@users.noreply.github.com>
2021-08-12 21:29:34 +0530

    dontknow

--- calc.js
+++ calc.js
@@ -65,5 +65,5 @@ function minus() {
 
 function backspace(){
     var result = document.getElementById("result").value;
-    document.getElementById("result").value=result.slice(0, result.length-1);;
+    document.getElementById("result").value=result.slice(0, result.length-1);
 }
