85d4db1eed98d672b8049ff92797ddc443da3b16
NakRex <40820072+NakRex@users.noreply.github.com>
2021-09-20 17:14:39 +0530

    added solve method and equals method

--- calc.js
+++ calc.js
@@ -3,64 +3,64 @@ var operand2 = '';
 
 function getOne() {
   let one = document.getElementById("one").value;
-  var result = (document.getElementById("result").value += one);
+  let result = (document.getElementById("result").value += one);
 }// console.log(plus);
 
 function getTwo() {
   let two = document.getElementById("two").value;
-  var result = (document.getElementById("result").value += two);
+  let result = (document.getElementById("result").value += two);
 }
 
 function getThree() {
   let three = document.getElementById("three").value;
-  var result = (document.getElementById("result").value += three);
+  let result = (document.getElementById("result").value += three);
 }
 
 function getFour() {
   let four = document.getElementById("four").value;
-  var result = (document.getElementById("result").value += four);
+  let result = (document.getElementById("result").value += four);
 }
 
 function getFive() {
   let five = document.getElementById("five").value;
-  var result = (document.getElementById("result").value += five);
+  let result = (document.getElementById("result").value += five);
 }
 
 function getSix() {
   let six = document.getElementById("six").value;
-  var result = (document.getElementById("result").value += six);
+  let result = (document.getElementById("result").value += six);
 }
 
 function getSeven() {
   let seven = document.getElementById("seven").value;
-  var result = (document.getElementById("result").value += seven);
+  let result = (document.getElementById("result").value += seven);
 }
 
 function getEight() {
   let eight = document.getElementById("eight").value;
-  var result = (document.getElementById("result").value += eight);
+  let result = (document.getElementById("result").value += eight);
 }
 
 function getNine() {
   let nine = document.getElementById("nine").value;
-  var result = (document.getElementById("result").value += nine);
+  let result = (document.getElementById("result").value += nine);
 }
 
 function getZero() {
   let zero = document.getElementById("zero").value;
-  var result = (document.getElementById("result").value += zero);
+  let result = (document.getElementById("result").value += zero);
 }
 
 function clearIt() {
-  var result = document.getElementById("result");
+  let result = document.getElementById("result");
   result.value = "";
 }
 
 function plus() {
-  var operand1 = document.getElementById("result").value
+  operand1 = document.getElementById("result").value
   let plus = document.getElementById("plus").value;
-  var result = (document.getElementById("result").value += plus);
-  console.log(typeof operand1);
+  let result = (document.getElementById("result").value += plus);
+  // console.log(operand1.length);
 }
 
 function minus() {
@@ -82,6 +82,24 @@ function backspace(){
     document.getElementById("result").value = result.slice(0, result.length-1);
 }
 function equals(){
-  var result = console.log(document.getElementById("result").value);
+  let result = document.getElementById("result").value;
+  console.log(operand1);
+  operand2 = result.slice(operand1.length+1, result.length)
+  console.log(operand2);
+  let operator = result.slice(operand1.length,operand1.length+1)
+  
+}
 
+function solve(a, b, operator) {
+  switch (operator) {
+    case "+": return a+b;
+    break;
+    case "-": return a-b;
+    break;
+    case "x" : return a*b;
+    break;
+    case "/": return a/b;
+    break;
+    default: return ""
+  }
 }
