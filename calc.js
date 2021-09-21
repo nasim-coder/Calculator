40359fca2f98fa4a6d7ebb7cdfe8b7cce6f34cbb
NakRex <40820072+NakRex@users.noreply.github.com>
2021-09-21 22:28:11 +0530

    updated

--- calc.js
+++ calc.js
@@ -1,11 +1,10 @@
-var operand1 = '';
-var operand2 = '';
-
+var operand1 = "";
+var operand2 = "";
 
 function getOne() {
   let one = document.getElementById("one").value;
   let result = (document.getElementById("result").value += one);
-}// console.log(plus);
+} 
 
 function getTwo() {
   let two = document.getElementById("two").value;
@@ -53,64 +52,65 @@ function getZero() {
 }
 
 function clearIt() {
-  document.getElementById("result").value="";
+  document.getElementById("result").value = "";
 }
 
 function plus() {
   operand1 = document.getElementById("result").value;
-  document.getElementById("result").value += document.getElementById("plus").value;
+  document.getElementById("result").value +=
+    document.getElementById("plus").value;
 }
 
 function minus() {
-  operand1 = document.getElementById("result").value
+  operand1 = document.getElementById("result").value;
   let minus = document.getElementById("minus").value;
   document.getElementById("result").value += minus;
 }
 
-function divide(){
-  operand1 = document.getElementById("result").value
+function divide() {
+  operand1 = document.getElementById("result").value;
   let divide = document.getElementById("divide").value;
-  document.getElementById("result").value += divide
+  document.getElementById("result").value += divide;
 }
 
-function multiply(){
+function multiply() {
   operand1 = document.getElementById("result").value;
   let multiply = document.getElementById("multiply").value;
   document.getElementById("result").value += multiply;
 }
-function backspace(){
-    var result = document.getElementById("result").value;
-    document.getElementById("result").value = result.slice(0, result.length-1);
+function backspace() {
+  var result = document.getElementById("result").value;
+  document.getElementById("result").value = result.slice(0, result.length - 1);
 }
-function equals(){
+function equals() {
   let result = document.getElementById("result").value;
-  operand2 = result.slice(operand1.length+1, result.length)
-  let operator = result.slice(operand1.length,operand1.length+1)
+  operand2 = result.slice(operand1.length + 1, result.length);
+  let operator = result.slice(operand1.length, operand1.length + 1);
   operand1 = parseInt(operand1);
   operand2 = parseInt(operand2);
   clearIt();
-  document.getElementById("result").value+=solve(operand1,operand2, operator);
+  document.getElementById("result").value += solve(
+    operand1,
+    operand2,
+    operator
+  );
 }
-
-
-
-
-
-
-
-
-
 
 function solve(a, b, operator) {
   switch (operator) {
-    case "+": return a+b;
-    break;
-    case "-": return a-b;
-    break;
-    case "x" : return a*b;
-    break;
-    case "/": return a/b;
-    break;
-    default: return "You are not good in Math."
+    case "+":
+      return a + b;
+      break;
+    case "-":
+      return a - b;
+      break;
+    case "x":
+      return a * b;
+      break;
+    case "/":
+      return a / b;
+      break;
+    default:
+      return "You would be good in Math.";
   }
 }
